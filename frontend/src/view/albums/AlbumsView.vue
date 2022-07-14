d72ee9eccb5c9842bdb1804c12ebed6e87a1bc56
wjbaker101 <willbaker10198@gmail.com>
2022-07-14 16:49:25 +0100

    More cleanup

--- frontend/src/view/albums/AlbumsView.vue
+++ frontend/src/view/albums/AlbumsView.vue
@@ -5,7 +5,6 @@
         <div class="images-container flex gap-small">
             <AlbumComponent :key="album.id" v-for="album in albums" :album="album" />
         </div>
-        <ModalComponent :style="'fullscreen'" />
     </div>
 </template>
 
@@ -13,12 +12,10 @@
 import { defineComponent, onMounted, ref } from 'vue';
 
 import ModalComponent from '@wjb/vue/component/ModalComponent.vue';
-import { useModal } from '@wjb/vue/use/modal.use';
 
 import LoginZeroStateComponent from '@/component/zero-state/LoginZeroStateComponent.vue';
 import ImageComponent from '@/view/albums/component/ImageComponent.vue';
-import ImageModalComponent from '@/view/albums/component/ImageModalComponent.vue';
-import AlbumComponent from './component/AlbumComponent.vue';
+import AlbumComponent from '@/view/albums/component/AlbumComponent.vue';
 
 import { api } from '@/api/api';
 import { useAuth } from '@/use/auth.use';
@@ -37,18 +34,8 @@ export default defineComponent({
 
     setup() {
         const auth = useAuth();
-        const modal = useModal();
 
         const authDetails = auth.value;
-
-        const images = [
-            {
-                url: 'https://i.imgur.com/SuEhwOQb.jpg',
-            },
-            {
-                url: 'https://i.imgur.com/SuEhwOQb.jpg',
-            },
-        ];
 
         const albums = ref<Array<IAlbum> | null>(null);
 
@@ -60,27 +47,16 @@ export default defineComponent({
                 return;
 
             albums.value = result;
-            console.log(albums.value)
         });
 
         return {
             authDetails,
-            images,
             albums,
-
-            onClick(i: string) {
-                modal.show({
-                    component: ImageModalComponent,
-                    componentProps: {},
-                });
-            },
         };
     },
 });
 </script>
 
 <style lang="scss">
-.albums-view {
-    --wjb_modal-content-background-colour: #222;
-}
+.albums-view {}
 </style>
